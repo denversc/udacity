b46e0e3c18ccc9b233785d913aa6d595ac793c58
Denver Coneybeare <denver.coneybeare@gmail.com>
2013-01-15 21:57:14 -0500

    cs215/unit02 up to prob01 added

---
+++ cs215/unit02/lect16_clique_growth_rate_my.py
@@ -0,0 +1,6 @@
+#
+# How many edges in a complete graph on n nodes?
+#
+
+def clique(n):
+    return (n * (n - 1)) / 2
---
+++ cs215/unit02/prob01_star_growth_my.py
@@ -0,0 +1,7 @@
+# Write a program that returns the number of edges
+# in a star network that has `n` nodes
+# http://www.udacity.com/view#Course/cs215/CourseRev/1/Unit/197001/Nugget/220004
+
+def star_network(n):
+    # return number of edges
+    return n - 1
